8c8581db39db52f656ec44d89c623e7a04854ed8
Joseph Orilogbon <logbon72@gmail.com>
2015-10-21 11:18:56 +0100

    - Added DEMO page.

---
+++ demo/demo.js
@@ -0,0 +1,14 @@
+(function(){
+  'use strict';
+
+  angular.module('recorderDemo', [
+    'angularAudioRecorder'
+  ])
+    .controller('DemoController', function($scope, $timeout){
+      console.log("Loaded");
+
+    }).config(function(recorderServiceProvider){
+      recorderServiceProvider.forceSwf(false);
+    });
+
+})();
