29184d3e6eab441bbc43f34847e7f80d9e8f01a2
Joseph Orilogbon <logbon72@gmail.com>
2015-10-21 13:35:04 +0100

    - completed basic demo

--- demo/demo.js
+++ demo/demo.js
@@ -7,8 +7,33 @@
     .controller('DemoController', function($scope, $timeout){
       console.log("Loaded");
 
-    }).config(function(recorderServiceProvider){
-      recorderServiceProvider.forceSwf(true);
-    });
+    })
+    .config(function(recorderServiceProvider){
+      recorderServiceProvider.forceSwf(window.location.search.indexOf('forceFlash') > -1)
+        .setSwfUrl('lib/recorder.swf');
+    })
+    .directive('canvasSizer', ['$timeout', '$window', function($timeout, $window){
+
+      return {
+        link: function(scope, element){
+          var resize = function(){
+            window.GLO = element.find('canvas');
+            element.find('canvas')
+              .attr('height', 300)
+              .attr('width', element[0].offsetWidth)
+            ;
+          };
+
+          $window.addEventListener('resize', function(){
+            resize();
+          });
+
+          $timeout(function(){
+            //just some delay for everything to settle
+            resize();
+          }, 1000);
+        }
+      };
+    }]);
 
 })();
